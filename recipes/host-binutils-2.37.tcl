3d319b3ec7e0f45129ea36be446a6b6f99b667ee
Aaron Klapatch <klapatchaaron@gmail.com>
2024-05-24 04:26:49 +0000

    Updated binutils recipe

--- recipes/host-binutils-2.37.tcl
+++ recipes/host-binutils-2.37.tcl
@@ -1,9 +1,10 @@
 set ver 2.37
 set name binutils
 set rep_info [dict create \
+	plat host \
 	name $name \
 	ver  $ver \
 	srcs "https://ftp.gnu.org/gnu/$name/$name-$ver.tar.xz" \
 	cd_dest "$name-$ver" \
-	make_flags "-j 4" \
+	make_flags "-j 1" \
 ]
