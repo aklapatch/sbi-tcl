bff22be256c3cc8082bf2c4e5e0951081d52d757
Aaron Klapatch <klapatchaaron@gmail.com>
2024-07-06 16:12:58 +0000

    Got mpc working

--- recipes/host-mpc-0.8.1.tcl
+++ recipes/host-mpc-0.8.1.tcl
@@ -1,15 +1,25 @@
 set ver 0.8.1
 set name mpc
-set gmp_dir [get_pkg_dir host-gmp-4.3.2]
-set mpfr_dir [get_pkg_dir host-mpfr-2.4.2]
 # NOTE: The multiprecision web site is slow last I used it, but the GNU ftp site doesn't have MPC 0.8.1.
 set rep_info [dict create \
 	plat "host" \
 	name $name \
 	ver  $ver \
 	srcs "https://www.multiprecision.org/downloads/$name-$ver.tar.gz" \
-	cd_dest "$name-$ver" \
-	make_flags "-j 4" \
 	build_needs {host-gmp-4.3.2 host-mpfr-2.4.2} \
-	cfg_flags "--disable-shared --with-gmp=$gmp_dir --with-mpfr=$mpfr_dir" \
 ]
+
+proc build {name ver inst_dir build_dir} {
+    autotools_build \
+        "$name-$ver" \
+        "--prefix=$inst_dir --with-pic --disable-shared --with-gmp=[get_pkg_dir host-gmp-4.3.2] --with-mpfr=[get_pkg_dir host-mpfr-2.4.2]" \
+        "-j 3"
+}
+
+proc check {pkg_name inst_dir build_dir} {
+    exec_stdout "make check -j 3"
+}
+
+proc install {pkg_name inst_dir build_dir} {
+    exec_stdout "make install"
+}
