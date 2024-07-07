0161c3fd424c95c4ed5456c84f1e77bbed47e9e6
Aaron Klapatch <klapatchaaron@gmail.com>
2024-07-07 20:17:11 +0000

    Changed tcl package a little

--- recipes/host-tcl-8.6.14.tcl
+++ recipes/host-tcl-8.6.14.tcl
@@ -6,7 +6,21 @@ set rep_info [dict create \
 	name $name \
 	ver  $ver \
 	srcs "http://prdownloads.sourceforge.net/tcl/tcl$ver-src.tar.gz" \
-	cd_dest "$name$ver/unix" \
-	make_flags "-j 3" \
-	cfg_flags "" \
 ]
+
+proc build {name ver inst_dir build_dir} {
+    autotools_build \
+        "$name$ver/unix" \
+        "--prefix=$inst_dir --disable-shared --enable-64bit" \
+        "-j 3"
+}
+
+proc check {pkg_name inst_dir build_dir} {
+    exec_stdout "make check -j 3"
+}
+
+proc install {pkg_name inst_dir build_dir} {
+    exec_stdout "make install"
+}
+
+
