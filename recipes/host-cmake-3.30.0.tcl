19ad91caf815f3a183edb6b2352946c5d60df822
Aaron Klapatch <klapatchaaron@gmail.com>
2024-07-12 02:24:11 +0000

    Got cmake working

--- recipes/host-cmake-3.30.0.tcl
+++ recipes/host-cmake-3.30.0.tcl
@@ -1,16 +1,20 @@
 set ver 3.30.0
 set name cmake
+set plat host
+set ::openssl "$plat-openssl-1.1.1w"
 set rep_info [dict create \
-	plat "host" \
+	plat $plat \
 	name $name \
 	ver  $ver \
+    build_needs "$::openssl" \
 	srcs "https://github.com/Kitware/CMake/releases/download/v$ver/cmake-$ver.tar.gz" \
 ]
 
 proc build {name ver inst_dir build_dir} {
+    set openssl_dir [get_pkg_dir $::openssl]
     autotools_build \
         "$name-$ver" \
-        "--prefix=$inst_dir " \
+        "--prefix=$inst_dir --parallel=3 -- -DOPENSSL_ROOT_DIR=$openssl_dir -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release" \
         "-j 3"
 }
 
